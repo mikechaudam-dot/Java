25dba0fd1e65e8727f341f0e4699e360c0d05fe0
“mickaeldam” <mikechaudam@gmail.com>
2025-11-11 21:04:53 +0100

    ejrcicios variables

---
+++ Ordenar3Numeros.java
@@ -0,0 +1,14 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ordenar3Numeros {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner (System.in);
+		
+		System 
+
+	}
+
+}
